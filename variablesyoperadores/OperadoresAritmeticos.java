b191d274466c813291debeffc53152a4c8740e4c
Antonio <nada@nada.com>
2021-02-23 19:01:34 +0100

    Varialbe sy Operadores

---
+++ variablesyoperadores/OperadoresAritmeticos.java
@@ -0,0 +1,28 @@
+package variablesyoperadores;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//  *  /   +   x
+		
+		// Resto %
+		
+		int n1=100;
+		System.out.println(n1%2);
+				
+		//++   --
+		//n=n+1
+		
+		int n2=10;
+		System.out.println(n2++);
+		System.out.println(n2);
+
+		//   +=,   *=, 
+		int v2=5;
+		System.out.println(v2*=4);  //v2=v2*4
+		
+		
+	}
+
+}
