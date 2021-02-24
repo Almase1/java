25dfff6c6a9ed8f9121948244669fe48aba31c44
Antonio <nada@nada.com>
2021-02-24 19:56:55 +0100

    Arrays y Tres en Raya

---
+++ arrays/ArraysString.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class ArraysString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String x="Hola";
+     
+   
+     
+     
+		
+		
+		
+		
+	}
+
+}
