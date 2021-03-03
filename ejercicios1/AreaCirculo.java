30514f3b6ad4d9d28ad891dc949d78ab9fe6a65f
Antonio <nada@nada.com>
2021-03-03 16:20:28 +0100

    Multiplo

--- ejercicios1/AreaCirculo.java
+++ ejercicios1/AreaCirculo.java
@@ -7,7 +7,7 @@ public class AreaCirculo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		double x=10.1;
 		Scanner scan = new Scanner(System.in);
 		scan.useLocale(Locale.ENGLISH);
 		System.out.println("Escribe el radio del círculo:");
