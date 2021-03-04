59090345375fe574902267f21ef19e21ba009913
Antonio <nada@nada.com>
2021-03-04 15:48:19 +0100

    ContarNumeros

--- ejerciciosBuclesArrays/ContarNegativos.java
+++ ejerciciosBuclesArrays/ContarNegativos.java
@@ -1,10 +1,33 @@
 package ejerciciosBuclesArrays;
 
+import java.util.Scanner;
+
 public class ContarNegativos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  
+		Scanner numero = new Scanner(System.in);
+
+		int num;
+		int cont = 0;
+		boolean marcador = true;
+
+		do {
+			System.out.println("Dime un numero");
+
+			num = numero.nextInt();
+
+			if (num >= 0) {
+				cont++;
+			}
+
+			else {
+				marcador = false;
+			}
+
+		} while (marcador);  //No hace falta poner la condicion
+		
+		System.out.println("Has introducido " + cont + " numeros");
 	}
 
 }
