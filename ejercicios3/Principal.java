9e2a1fcc21ab559845ee89f0a4939dbf3224574b
Antonio <nada@nada.com>
2021-03-08 19:19:30 +0100

    Varios

---
+++ ejercicios3/Principal.java
@@ -0,0 +1,21 @@
+package ejercicios3;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*	Factura fac1=new Factura();
+			fac1.altafactura();
+			fac1.verfactura();
+			fac1.modfactura1("precio", 100);
+			fac1.verfactura();*/
+		
+		Figura fig1=new Figura();
+		fig1.setLado1(10000);
+		fig1.setLado2(20);
+		System.out.println(fig1.area());
+		
+	}
+
+}
---
+++ herencia/Principal.java
@@ -0,0 +1,13 @@
+package herencia;
+
+public class Principal {
+    public static void main(String[] args) {
+		
+	
+	
+	Padre p1=new Padre(1,"hola");
+          
+    System.out.println(p1.getDato());
+    System.out.println(p1.getCod());
+    }
+}
