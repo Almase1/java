9f1315c6b157a00097880a3a3f2018b484fbee2e
Antonio <nada@nada.com>
2021-03-12 16:16:59 +0100

    Excepcion Array

--- excepciones/PosicionArrayIncorrecta.java
+++ excepciones/PosicionArrayIncorrecta.java
@@ -3,18 +3,21 @@ package excepciones;
 public class PosicionArrayIncorrecta {
 
 	public static void main(String[] args) {
-       
-		int array1[]= {2,4,1,6};
-		
+
+		// Defino el array
+		int array1[] = { 2, 4, 1, 6 };
+
+		// Bloque Try-Catch
 		try {
-		System.out.println(array1[4]);
+			System.out.println(array1[6]);
+			System.out.println(10 / 0);
+		} catch (ArrayIndexOutOfBoundsException mi_error) {
+			System.out.println("Me parece que no existe esa posicion");
+		} catch (ArithmeticException e1) {
+			System.out.println("Has dividido por CERO");
+		} catch (Exception e2) {
+			System.out.println("Error desconocido:" + e2.getMessage());
 		}
-		catch (Exception mi_error) {
-			System.out.println("Ma parece que no existe esa posicion");
-		}
-		
-		
-		
 	}
 
 }
